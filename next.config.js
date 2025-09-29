0efea66816f97e96b549d6f134867058f3a12ac9
SkeletalReality <connorglasgow4x5@gmail.com>
2025-09-29 12:44:31 -0700

    Remove submodule

--- next.config.js
+++ next.config.js
@@ -31,10 +31,10 @@ const config = withPWA({
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 });
 
 export default config;
